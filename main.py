a94874111c44fc5acaa19d50f69c98f5e6a37ff7
Anna Slian <annasli378@student.polsl.pl>
2024-04-03 19:47:37 +0200

    update

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from scipy.stats import hmean
 from functions import get_mask, get_blobs, get_widths_bwd
 import scipy.io
-## Read mask
+
+# Read mask
 number_experts_mes = [27, 18, 17, 9, 14, 18, 4, 15, 8, 8, 14, 20, 14, 20, 11, 17, 19, 27, 17, 26]
 obr_num = 1
-pth = "D:/GRUBA_MEMBRANA_PROJEKT/Proj_1/CALE_OBRAZKI/Masks/1_Mask.mat"
+pth = f"D:/GRUBA_MEMBRANA_PROJEKT/Proj_1/CALE_OBRAZKI/Masks/{obr_num}_Mask.mat"
 MASK = scipy.io.loadmat(pth)
 mask = MASK['ABCD']
 
@@ -34,6 +35,6 @@ h_mean = hmean(widths_nm)
 h_mean_r20 = hmean(widths_nm[r])
 
 print("Harmonic mean: ", h_mean)
-print("Harmonic mean with random 20 samples: ", h_mean_r20)
+print(f"Harmonic mean with random {N} samples: {h_mean_r20}")
 h_mean_r20 = hmean(widths_nm[r])
 
